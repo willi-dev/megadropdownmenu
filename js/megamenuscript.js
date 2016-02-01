ea5b8647492f9ae32eb8470dd9c9cd662a5ae2b6
willi-dev <willi.ilmukomputer@gmail.com>
2016-02-01 13:41:19 +0700

    megamenuscript.js nextprev funct
    megamenuscript.js
    add next function;
    add prev function;

--- js/megamenuscript.js
+++ js/megamenuscript.js
@@ -32,58 +32,107 @@ jQuery(document).ready(function() {
     var urlWP = ajax_script.ajaxurl;
     // var currentPage;
     
+    nextMegaMenu();
+    prevMegaMenu();
+
     /*
      * next content post
      */
-    jQuery('.next_megamenu').click(function(e){
-        e.preventDefault();
-        var categoryId = jQuery(this).data('cat');     
-        var totalPages = jQuery(".total_pages-" +categoryId).val();
-        var foundPosts = jQuery(".found_posts-" +categoryId).val();
-        var postsPerPage = jQuery(".posts_per_page-" +categoryId).val();
-        jQuery.ajax({
-            type: 'POST',
-            url: urlWP,
-            data: {
-                action: 'getNextPage', 
-                total_pages: totalPages,
-                found_posts: foundPosts,
-                posts_per_page: postsPerPage
-            },
-            success: function(data){
-                alert(data);
+    function nextMegaMenu(){
+        jQuery('.next_megamenu').click(function(e){
+            e.preventDefault();
+            var noItem = jQuery(this).data('item');  
+            var totalPages = jQuery(".total_pages-" +noItem).val();
+            var foundPosts = jQuery(".found_posts-" +noItem).val();
+            var postsPerPage = jQuery(".posts_per_page-" +noItem).val();
+            var currentPage = jQuery(".current_page-" +noItem).val();
+            var categoryId = jQuery(".category_id-" +noItem).val();
+            jQuery.ajax({
+                type: 'POST',
+                url: urlWP,
+                data: {
+                    action: 'getNextPage', 
+                    no_item: noItem,
+                    category_id: categoryId,
+                    total_pages: totalPages,
+                    found_posts: foundPosts,
+                    posts_per_page: postsPerPage,
+                    current_page: currentPage,
+                    type: 'next'
+                },
+                success: function(data){
+                    // alert(data);
+                    jQuery(".block_inner_megamenu-"+categoryId+"-"+noItem).remove().fadeOut(function(){
+                        // remove style in prev link
+                        jQuery("#prev-"+categoryId+"-"+noItem).removeAttr( "style" );
 
-            }
-        });
-    })
+                        // add next content to .block_megamenu-[cat id]-[no item]
+                        jQuery(".block_megamenu-"+categoryId+"-"+noItem).prepend(data).fadeIn('slow', function(){
+                            var cPage = jQuery(".current_page-"+noItem).val();
+                            var tPages = jQuery(".total_pages-"+noItem).val();
+                            if(cPage == tPages){
+                                jQuery("#next-"+categoryId+"-"+noItem).css("pointer-events", 'none');
+                                jQuery("#next-"+categoryId+"-"+noItem).css("cursor", 'default');
+                                jQuery("#next-"+categoryId+"-"+noItem).css("color", '#ccc');
+                            }
+                        });  
+                                           
+                    });
+                    
+
+                }
+            });
+        })
+    }
+    
 
     /*
      * prev content post
      */
-    jQuery('.prev_megamenu').click(function(e){
-        e.preventDefault();
-        var categoryId = jQuery(this).data('cat');     
-        var totalPages = jQuery(".total_pages-" +categoryId).val();
-        var foundPosts = jQuery(".found_posts-" +categoryId).val();
-        var postsPerPage = jQuery(".posts_per_page-" +categoryId).val();
-        jQuery.ajax({
-            type: 'POST',
-            url: urlWP,
-            data: {
-                action: 'getPrevPage', 
-                total_pages: totalPages,
-                found_posts: foundPosts,
-                posts_per_page: postsPerPage
-            },
-            success: function(data){
-                // alert(data);
-            },
-            error: function(){
+    function prevMegaMenu(){
+        jQuery('.prev_megamenu').click(function(e){
+            e.preventDefault();
+            var noItem = jQuery(this).data('item');  
+            var totalPages = jQuery(".total_pages-" +noItem).val();
+            var foundPosts = jQuery(".found_posts-" +noItem).val();
+            var postsPerPage = jQuery(".posts_per_page-" +noItem).val();
+            var currentPage = jQuery(".current_page-" +noItem).val();
+            var categoryId = jQuery(".category_id-" +noItem).val();
+            jQuery.ajax({
+                type: 'POST',
+                url: urlWP,
+                data: {
+                    action: 'getPrevPage', 
+                    no_item: noItem,
+                    category_id: categoryId,
+                    total_pages: totalPages,
+                    found_posts: foundPosts,
+                    posts_per_page: postsPerPage,
+                    current_page: currentPage,
+                    type: 'prev'
+                },
+                success: function(data){
+                    jQuery(".block_inner_megamenu-"+categoryId+"-"+noItem).remove().fadeOut(function(){
+                        jQuery(".block_megamenu-"+categoryId+"-"+noItem).prepend(data).fadeIn('slow', function(){
+                            var cPage = jQuery(".current_page-"+noItem).val();
+                            var tPages = jQuery(".total_pages-"+noItem).val();
+                            if(cPage == '1'){
+                                jQuery("#prev-"+categoryId+"-"+noItem).css("pointer-events", 'none');
+                                jQuery("#prev-"+categoryId+"-"+noItem).css("cursor", 'default');
+                                jQuery("#prev-"+categoryId+"-"+noItem).css("color", '#ccc');
+                                // remove style in prev link
+                                jQuery("#next-"+categoryId+"-"+noItem).removeAttr( "style" );
+                            }
+                        });  
+                    });
+                },
+                error: function(){
 
-            }
+                }
+            })
         })
-    })
-
+    }
+    
 
 });
 
